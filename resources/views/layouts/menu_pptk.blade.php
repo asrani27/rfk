5597cf239920f27579d4b5ab2ad118edfc6ce085
asrani <asrani.27@gmail.com>
2022-08-26 08:05:58 +0800

    f

--- resources/views/layouts/menu_pptk.blade.php
+++ resources/views/layouts/menu_pptk.blade.php
@@ -1,7 +1,7 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column text-sm" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-            <a href="/berandabidang" class="nav-link {{Request::is('berandabidang') ? 'active' : ''}}">
+            <a href="/berandapptk" class="nav-link {{Request::is('berandabidang') ? 'active' : ''}}">
                 <i class="nav-icon fas fa-home"></i>
                 <p>
                     BERANDA
